cc9c98ce6fa9479ce7388d4657a737c7dab4be82
christdevtech <michael@christdev.com>
2026-01-29 14:53:37 +0100

    refactor(Header): restructure config into tabs for better organization
    - Move top bar configuration into "Header Navigation" tab
    - Move navigation items into "Top Bar & Socials" tab
    - Maintain all existing functionality while improving admin UI structure

--- src/Header/config.ts
+++ src/Header/config.ts
@@ -10,97 +10,112 @@ export const Header: GlobalConfig = {
   },
   fields: [
     {
-      name: 'topBar',
-      type: 'group',
-      label: 'Top Bar',
-      fields: [
+      type: 'tabs',
+      tabs: [
         {
-          name: 'enabled',
-          type: 'checkbox',
-          label: 'Enable Top Bar',
-          defaultValue: true,
-        },
-        {
-          name: 'contactDetails',
-          type: 'array',
-          label: 'Contact Details',
+          label: 'Header Navigation',
           fields: [
             {
-              name: 'icon',
-              type: 'select',
-              options: [
-                { label: 'Map Pin', value: 'mapPin' },
-                { label: 'Phone', value: 'phone' },
-                { label: 'Email', value: 'mail' },
-                { label: 'Clock', value: 'clock' },
+              name: 'navItems',
+              type: 'array',
+              fields: [
+                link({
+                  appearances: false,
+                }),
+                {
+                  name: 'hasSubMenu',
+                  type: 'checkbox',
+                  label: 'Has Sub-menu',
+                  defaultValue: false,
+                },
+                {
+                  name: 'subMenuLinks',
+                  type: 'array',
+                  label: 'Sub-menu Links',
+                  admin: {
+                    condition: (_, siblingData) => siblingData?.hasSubMenu,
+                  },
+                  fields: [
+                    link({
+                      appearances: false,
+                    }),
+                  ],
+                },
               ],
-              required: true,
-            },
-            {
-              name: 'text',
-              type: 'text',
-              required: true,
-            },
-            {
-              name: 'link', // Optional link for the contact item
-              type: 'text',
+              maxRows: 12,
             },
           ],
         },
         {
-          name: 'socialLinks',
-          type: 'array',
-          label: 'Social Links',
+          label: 'Top Bar & Socials',
           fields: [
             {
-              name: 'platform',
-              type: 'select',
-              options: [
-                { label: 'Facebook', value: 'facebook' },
-                { label: 'Twitter', value: 'twitter' },
-                { label: 'Instagram', value: 'instagram' },
-                { label: 'LinkedIn', value: 'linkedin' },
-                { label: 'YouTube', value: 'youtube' },
+              name: 'topBar',
+              type: 'group',
+              label: 'Top Bar',
+              fields: [
+                {
+                  name: 'enabled',
+                  type: 'checkbox',
+                  label: 'Enable Top Bar',
+                  defaultValue: true,
+                },
+                {
+                  name: 'contactDetails',
+                  type: 'array',
+                  label: 'Contact Details',
+                  fields: [
+                    {
+                      name: 'icon',
+                      type: 'select',
+                      options: [
+                        { label: 'Map Pin', value: 'mapPin' },
+                        { label: 'Phone', value: 'phone' },
+                        { label: 'Email', value: 'mail' },
+                        { label: 'Clock', value: 'clock' },
+                      ],
+                      required: true,
+                    },
+                    {
+                      name: 'text',
+                      type: 'text',
+                      required: true,
+                    },
+                    {
+                      name: 'link', // Optional link for the contact item
+                      type: 'text',
+                    },
+                  ],
+                },
+                {
+                  name: 'socialLinks',
+                  type: 'array',
+                  label: 'Social Links',
+                  fields: [
+                    {
+                      name: 'platform',
+                      type: 'select',
+                      options: [
+                        { label: 'Facebook', value: 'facebook' },
+                        { label: 'Twitter', value: 'twitter' },
+                        { label: 'Instagram', value: 'instagram' },
+                        { label: 'LinkedIn', value: 'linkedin' },
+                        { label: 'YouTube', value: 'youtube' },
+                      ],
+                      required: true,
+                    },
+                    {
+                      name: 'url',
+                      type: 'text',
+                      required: true,
+                    },
+                  ],
+                },
               ],
-              required: true,
-            },
-            {
-              name: 'url',
-              type: 'text',
-              required: true,
             },
           ],
         },
       ],
-    },
-    {
-      name: 'navItems',
-      type: 'array',
-      fields: [
-        link({
-          appearances: false,
-        }),
-        {
-          name: 'hasSubMenu',
-          type: 'checkbox',
-          label: 'Has Sub-menu',
-          defaultValue: false,
-        },
-        {
-          name: 'subMenuLinks',
-          type: 'array',
-          label: 'Sub-menu Links',
-          admin: {
-            condition: (_, siblingData) => siblingData?.hasSubMenu,
-          },
-          fields: [
-            link({
-              appearances: false,
-            }),
-          ],
-        },
-      ],
-      maxRows: 6,
     },
   ],
   hooks: {
